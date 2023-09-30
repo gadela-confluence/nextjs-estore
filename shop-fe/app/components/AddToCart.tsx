baa580b808b36ac478b1f4034d6399f81727e288
ravi4target <ravi.gadela@target.com.au>
2023-09-30 22:13:50 +1000

    Client Side Rendering and Server Side Rendering. In nextjs server side rendering is default

---
+++ shop-fe/app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('button clicked')}> Add To Cart </button>    
+    </div>
+  )
+}
+
+export default AddToCart
---
+++ shop-fe/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+    <h1>ProductCard</h1>
+    <AddToCart></AddToCart>
+    </div>
+  )
+}
+
+export default ProductCard
