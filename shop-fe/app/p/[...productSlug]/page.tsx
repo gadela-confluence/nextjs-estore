d7495d1b4706e915d0893d1bc0025b63e28587a5
ravi4target <ravi.gadela@target.com.au>
2023-10-01 16:49:32 +1100

    Ready Query Parameters

--- shop-fe/app/p/[...productSlug]/page.tsx
+++ shop-fe/app/p/[...productSlug]/page.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 interface Props {
     params: {productSlug: string[]}
+    searchParams: {sortOrder: string}
 }
 
-const ProductDetailsPage = ( {params: {productSlug}}:Props) => {
+const ProductDetailsPage = ( {params: {productSlug},searchParams: {sortOrder}}:Props) => {
   return (
-    <div>ProductDetailsPage {productSlug} </div>
+    <div>ProductDetailsPage {productSlug} SortOrder: {sortOrder} </div>
   )
 }
 
