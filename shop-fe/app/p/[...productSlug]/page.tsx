e53a342b5f8fb8f27fadbb2a9e006b7615f4e032
ravi4target <ravi.gadela@target.com.au>
2023-10-02 12:33:54 +1100

    Get Product from Product API using Axios

--- shop-fe/app/p/[...productSlug]/page.tsx
+++ shop-fe/app/p/[...productSlug]/page.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import axios from 'axios'
+
 
 interface Props {
     params: {productSlug: string[]}
     searchParams: {sortOrder: string}
 }
 
-const ProductDetailsPage = ( {params: {productSlug},searchParams: {sortOrder}}:Props) => {
+const ProductDetailsPage = async ( {params: {productSlug},searchParams: {sortOrder}}:Props) => {
+
+  console.log(productSlug);
+  const url =  `http://127.0.0.1:5001/occ/rest/v2/target/products/${productSlug}`;
+
+
+  try {
+		const { data } = await axios.get(url)
+    console.log(data);
+
+	} catch (error) { 
+    console.log(error)
+  }
+  
   return (
-    <div>ProductDetailsPage {productSlug} SortOrder: {sortOrder} </div>
+    <div>ProductDetailsPage {productSlug}</div>
   )
 }
+
+
 
 export default ProductDetailsPage
